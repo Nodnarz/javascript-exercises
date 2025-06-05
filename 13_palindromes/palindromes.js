c643958432d0db13543292f52262418cbee01c04
nodnarz <brandonmandras@gmail.com>
2025-06-05 12:15:48 +1000

    Complete exercises 11 - 16
    Struggled a lot with 16, ended up peeking

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/\s/g, '')
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    string = string.toLowerCase()
+    if (string.split("").reverse().join("") === string) return true
+    else return false
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number == 0 || number == 1) return Number(number)
+    if (number < 0) return "OOPS"
+    return fibonacci(number - 1) + fibonacci(number - 2)
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+/*
+ 0, 1, 1, 2, 3, 5, 8, 13,...
+*/
