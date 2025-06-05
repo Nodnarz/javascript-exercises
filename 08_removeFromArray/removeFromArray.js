6f3ddea0dbd91f56d298c350c3eb47709477fad9
nodnarz <brandonmandras@gmail.com>
2025-06-05 10:00:48 +1000

    Complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...extra) {
+    let newArray = []
+
+    array.forEach((element) => {
+        if (!extra.includes(element)) {
+            newArray.push(element)
+        }
+    });
+
+    return newArray
+    
 };
 
 // Do not edit below this line
