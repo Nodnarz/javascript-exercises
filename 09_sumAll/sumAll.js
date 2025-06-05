430c6922dfa18aafb201d8ebb1aab9779af2e415
nodnarz <brandonmandras@gmail.com>
2025-06-05 10:43:11 +1000

    Complete exercises 09 and 10

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let result = 0
+    if ((a || b) < 0) return "ERROR"
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"
+    if ((typeof(a) != "number") || typeof(b) != "number") return "ERROR"
+    
+    if (a > b) {
+        let temp = 0
+        temp = a
+        a = b
+        b = temp
+    }
 
+    for (let i = a; i <= b; i++) {
+        result += i;
+    }
+
+    return result
 };
 
 // Do not edit below this line
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //non-century and divisable by 4
+    if (year % 4 == 0 && year % 100 != 0) return true
 
+    //centuries covered
+    else if (year % 100 == 0 && year % 400 == 0) return true
+    
+    else return false
 };
 
 // Do not edit below this line
